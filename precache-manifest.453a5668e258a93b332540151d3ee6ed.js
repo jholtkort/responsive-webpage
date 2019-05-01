94666980312b5999fe71aac7c5e0d4ed6a22a7bc
Josh Holtkort <jholtkort@utexas.edu>
2019-05-01 15:39:07 -0500

    Updates

---
+++ precache-manifest.453a5668e258a93b332540151d3ee6ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "195b9054f545a877607377a15b0a031e",
+    "url": "/responsive-webpage/index.html"
+  },
+  {
+    "revision": "6d0c71fa6196e82a733d",
+    "url": "/responsive-webpage/static/css/main.a20a261b.chunk.css"
+  },
+  {
+    "revision": "228e9e38ae5cc2b3a6e4",
+    "url": "/responsive-webpage/static/js/2.eeb5e214.chunk.js"
+  },
+  {
+    "revision": "6d0c71fa6196e82a733d",
+    "url": "/responsive-webpage/static/js/main.1c490dd5.chunk.js"
+  },
+  {
+    "revision": "c2abaf68d31ca2065bdd",
+    "url": "/responsive-webpage/static/js/runtime~main.b0ca5604.js"
+  }
+]);
